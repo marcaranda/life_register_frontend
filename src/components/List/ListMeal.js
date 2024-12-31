1ea26a8c3eaa0ebdd25001d28fe8f01c7e444833
marcaranda <marc.aranda@estudiantat.upc.edu>
2024-12-31 08:42:51 +0100

    foodapi edamam macros

--- src/components/List/ListMeal.js
+++ src/components/List/ListMeal.js
@@ -5,6 +5,19 @@ import '../../styles/components/List/ListMeal.css';
 
 function Meal ({ mealObj, index }) {
   const [showDetails, setShowDetails] = useState(false);
+  const ingredients = mealObj["meal"];
+  const macros = mealObj["macros"];
+
+  const roundValue = (value) => {
+    const [number, unit] = value.split(' ');
+    const roundedNumber = Math.round(parseFloat(number));
+    return `${roundedNumber} ${unit}`;
+  };
+
+  const capitalizeFirstLetter = (str) => {
+    if (!str) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
   return (
     <div className='item'>
@@ -15,11 +28,36 @@ function Meal ({ mealObj, index }) {
         </p>
       </div>
       {showDetails && (
-        <div>
-          {mealObj.map((meal, index) => (
-            <p key={index}>{meal.name} {meal.quantity} {meal.unit}</p>
-          ))}
-        </div>
+        <>
+          <div className='ingredients'>
+            <ul>
+              {ingredients.map((meal, index) => (
+                <li key={index}>{capitalizeFirstLetter(meal.name)} {meal.quantity} {meal.unit}</li>
+              ))}
+            </ul>
+          </div>
+          <p className='line'></p>
+          <div className='macros'>
+            <ul>
+              <li key="calories">Calorías: {roundValue(macros["calorias"])}</li>
+              <li key="carbohydrates">Carbohidratos:
+                <ul>
+                  {Object.entries(macros["carbohidratos"]).map(([key, value]) => (
+                    <li key={key}>{key}: {roundValue(value)}</li>
+                  ))}
+                </ul>
+              </li>
+              <li key="fats">Grasas:
+                <ul>
+                  {Object.entries(macros["grasas"]).map(([key, value]) => (
+                    <li key={key}>{key}: {roundValue(value)}</li>
+                  ))}
+                </ul>
+              </li>
+              <li key="proteins">Proteínas: {roundValue(macros["proteinas"])}</li>
+            </ul>
+          </div>
+        </>
       )}
     </div>
   )
